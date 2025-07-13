d43733cc82747ea65908dea88b164040f69b635f
Atharva <atharvaanbhule@gmail.com>
2025-07-13 19:18:42 +0530

    first commit

--- src/components/ChakraShape3D.tsx
+++ src/components/ChakraShape3D.tsx
@@ -1,27 +1,30 @@
 // ChakraShape3D.tsx
 import React from "react";
 import { useFrame } from "@react-three/fiber";
-import { Sphere, Torus, Icosahedron } from "@react-three/drei";
-
-const chakraShapes = [
-  Sphere,        // Root Chakra
-  Torus,         // Sacral
-  Icosahedron,   // Solar Plexus
-  Sphere,        // Heart
-  Torus,         // Throat
-  Icosahedron,   // Third Eye
-  Sphere         // Crown
-];
 
 const ChakraShape3D = ({ index, color }: { index: number; color: string }) => {
-  const Shape = chakraShapes[index % chakraShapes.length];
+  const shapeIndex = index % 3; // 0: Sphere, 1: Torus, 2: Icosahedron
+
+  let geometry: JSX.Element;
+  switch (shapeIndex) {
+    case 0:
+      geometry = <sphereGeometry args={[1, 32, 32]} />;
+      break;
+    case 1:
+      geometry = <torusGeometry args={[1, 0.4, 16, 100]} />;
+      break;
+    case 2:
+      geometry = <icosahedronGeometry args={[1, 0]} />;
+      break;
+    default:
+      geometry = <sphereGeometry args={[1, 32, 32]} />;
+  }
 
   return (
-    <group rotation={[0, 0, 0]}>
-      <Shape args={[1, 32, 32]}>
-        <meshStandardMaterial color={color} roughness={0.4} metalness={0.5} />
-      </Shape>
-    </group>
+    <mesh rotation={[0, 0, 0]}>
+      {geometry}
+      <meshStandardMaterial color={color} roughness={0.4} metalness={0.5} />
+    </mesh>
   );
 };
 
